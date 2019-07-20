9ae2d7c81414d624bfa7582b13bb6efe66aea99d
ms32g13 <33421596+ms32g13@users.noreply.github.com>
2019-07-20 12:25:01 +0100

    mainly b_sgamma modified
    >b_sgamma.py
    >> added a global parameter ''mass_differ'' to set mass difference between two charged Higgs masses.
    >> changed the Plot_5() function now it can reproduce figure 5 in arXiv: 1605.05881. Added several titles, legends, for all Plot_{3,4,5} functions.
    >> removed the functions of (X, Y, Z)_{2,3}, complexfunction, and complex3function from exercise.py. They can call back.
    > can't remember whether any other changes
    >> see comments if any crucial changes

--- b_sgamma.py
+++ b_sgamma.py
@@ -13,11 +13,12 @@ from scipy import special as sp
 from invariant import *
 from matplotlib.ticker import MultipleLocator
 #from eehh import mhch
-from exercise import xyfun,xyfun3,yfun,yfun3,U,X2,X3,Y2,Y3,Z2,Z3,complexyfunction,complexyfunction3
+#from exercise import xyfun,xyfun3,yfun,yfun3,U,X2,X3,Y2,Y3,Z2,Z3,complexyfunction,complexyfunction3
 #import exercise as ex
-print('xyfun',np.array(xyfun))
-print('U',U(- PI/2.1,2,20,0.0)[0][2] / U(- PI/2.1,2,20,0.0)[0][0])
-print('X3',X3(- PI/2.1,2,20,0.0),Y3(- PI/2.1,2,20,0.0))
+#print('xyfun',np.array(xyfun))
+#print('U',U(- PI/2.1,2,20,0.0)[0][2] / U(- PI/2.1,2,20,0.0)[0][0])
+#print('X3',X3(- PI/2.1,2,20,0.0),Y3(- PI/2.1,2,20,0.0))
+mass_differ  = 80   # charged Higgs mass diference
 axs = np.arange(1, 40, 1)
 def array4():
     tuple_array = []
@@ -589,12 +590,18 @@ for m in np.arange(0,len(charHm_100)):
 def Plot_3():#Figure 3 DOI: 10.1142/S0217751X17501457
     x_axis = np.array([ i for i in np.arange(100,1020,20)] )
     print(x_axis,type(x_axis),x_axis.shape)
-    y11_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,[1.0],[-1.0],[0],[0]) )
-    y12_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,[1.0/2.0],[-1.0/4.0],[0],[0]) )
-    y130_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,[1.0/30.0],[-1.0/900],[0],[0]) )
-    y21_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,[1.0],[1.0],[0],[0]) )
-    y22_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,[1.0/2.0],[1.0],[0],[0]) )
-    y230_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,[1.0/30.0],[1.0],[0],[0]) )
+    y11_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0],[-1.0],[0],[0]) )
+    y12_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0/2.0],[-1.0/4.0],[0],[0]) )
+    y130_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0/30.0],[-1.0/900],[0],[0]) )
+    y21_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0],[1.0],[0],[0]) )
+    y22_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0/2.0],[1.0],[0],[0]) )
+    y230_axis = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0/30.0],[1.0],[0],[0]) )
     plt.axis([100.0, 1000.0, 1.0, 7.0])
     plt.plot(x_axis,y11_axis / (1e-4))
     plt.plot(x_axis,y12_axis / (1e-4))
@@ -605,6 +612,7 @@ def Plot_3():#Figure 3 DOI: 10.1142/S0217751X17501457
     print('type I tanbeta = 1',y11_axis / (1e-4))
     plt.xlabel('$M_{H^{\pm}}$')
     plt.ylabel('BR($\\bar{B} \\to X_{s} \gamma$) $\\times 10^{4}$')
+    plt.title('Figure 3: BR($\\bar{B} \\to X_{s} \gamma$) VS. $M_{H^{\pm}_{1}}$ ' )
     plt.legend(('Type I tan$\\beta =$ 1', 'Type I tan$\\beta =$ 2', 'Type I tan$\\beta =$ 30',\
             'Type II tan$\\beta =$ 1', 'Type II tan$\\beta =$ 2', 'Type II tan$\\beta =$ 30'),
            loc='upper right', shadow=True,prop={'size': 7.8})
@@ -621,17 +629,17 @@ def Plot_8_9():
     print('X = 2exp(i phi)',XYexpim_axis,len(XYexpim_axis))
    #mlx = MultipleLocator(1)
    #mly = MultipleLocator(0.25)
-    y48imx_axis = BR_B_Xs_gamma(4.8,mhch,mhch,100 ,\
+    y48imx_axis = BR_B_Xs_gamma(4.8,mhch,mhch,mhch + mass_differ ,\
                         [1.0],XYimx_axis,[0.0],[0.0])
-    y24imx_axis = BR_B_Xs_gamma(2.4,mhch,mhch,100,\
+    y24imx_axis = BR_B_Xs_gamma(2.4,mhch,mhch,mhch + mass_differ,\
                         [1.0],XYimx_axis,[0.0],[0.0])
-    y96imx_axis = BR_B_Xs_gamma(9.6,mhch,mhch,100,\
+    y96imx_axis = BR_B_Xs_gamma(9.6,mhch,mhch,mhch + mass_differ,\
                         [1.0],XYimx_axis,[0.0],[0.0])
-    y48phi_axis = BR_B_Xs_gamma(4.8,mhch,mhch,100 ,\
+    y48phi_axis = BR_B_Xs_gamma(4.8,mhch,mhch,mhch + mass_differ ,\
                         [0.5],XYexpim_axis,[0.0],[0.0])
-    y24phi_axis = BR_B_Xs_gamma(2.4,mhch,mhch,100,\
+    y24phi_axis = BR_B_Xs_gamma(2.4,mhch,mhch,mhch + mass_differ,\
                         [0.5],XYexpim_axis,[0.0],[0.0])
-    y96phi_axis = BR_B_Xs_gamma(9.6,mhch,mhch,100,\
+    y96phi_axis = BR_B_Xs_gamma(9.6,mhch,mhch,mhch + mass_differ,\
                         [0.5],XYexpim_axis,[0.0],[0.0])
 
     plt.xlim(-7, 7)
@@ -667,14 +675,17 @@ def Plot_4() :
     X1_array =  1.0/ 2.0  * np.cos(PI/4.0) 
     X2_array = - 1.0/2.0 * np.sin(PI/4.0)
     Y2_array = 1.0/2.0 * np.sin(PI/4.0)
-    y12_2hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 80,[1.0/2.0],[-1.0/4.0],[0],[0]) )
-    y12_3hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 80,\
+    y12_2hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0/2.0],[-1.0/4.0],[0],[0]) )
+    y12_3hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
                      [Y1_array],[X1_array * Y1_array],\
                      [Y2_array],[X2_array * Y2_array]) )
     plt.plot(x_axis,y12_2hdm / (1e-4))
     plt.plot(x_axis,y12_3hdm / (1e-4))
-    plt.xlabel('$M_{H^{\pm}}$')
+    plt.xlabel('$M_{H^{\pm}_{1}}$')
     plt.ylabel('BR($\\bar{B} \\to X_{s} \gamma$) $\\times 10^{4}$')
+    plt.title('Figure 4:$\\theta = \\pi$/4,$M_{H^{\pm}_{2}}$ = $M_{H^{\pm}_{1}}$ +' \
+                   + str(mass_differ)+ 'GeV' )
     plt.legend(('Type I tan$\\beta =$ 2 2HDM', 'Type I tan$\\beta =$ 2 3HDM '))
 #    for n in np.arange(0,len(axs)):
         
@@ -691,6 +702,9 @@ def Plot_5() :
 #    print('II 2HDM tanbeta = 2',BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,\
 #                                              [1.0/2.0],[1.0],[0],[0])\
 #            )
+    y22_2hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
+                                      [1.0/2.0],[1.0],[0],[0]) )
+    plt.plot(x_axis,y22_2hdm / (1e-4))
     for n in np.array([2,4,7,10,20]):
         
         X1_array =  - 2.0  * np.cos(- PI/4.0) - n / (1/np.sqrt(5)) * np.sin(- PI/4.0)
@@ -698,22 +712,26 @@ def Plot_5() :
         X2_array =  2.0  * np.sin(- PI/4.0) - n / (1/np.sqrt(5)) * np.cos(- PI/4.0)
         Y2_array = 1.0/2.0 * np.sin(-PI/4.0)
         
-        y22_2hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 50,[1.0/2.0],[1.0],[0],[0]) )
-        y12_3hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 50,\
+        y12_3hdm = np.array(BR_B_Xs_gamma(mb,mw,x_axis,x_axis + mass_differ,\
                      [Y1_array],[X1_array * Y1_array],\
                      [Y2_array],[X2_array * Y2_array]) )
-        plt.plot(x_axis,y22_2hdm / (1e-4))
-        plt.plot(x_axis,y12_3hdm / (1e-4))
-        plt.xlabel('$M_{H^{\pm}}$')
-        plt.ylabel('BR($\\bar{B} \\to X_{s} \gamma$) $\\times 10^{4}$')
         
+        plt.plot(x_axis,y12_3hdm / (1e-4))
+        plt.xlabel('$M_{H^{\pm}_{1}}$')
+        plt.ylabel('BR($\\bar{B} \\to X_{s} \gamma$) $\\times 10^{4}$')
+        plt.title('Figure 5:$\\theta = -\\pi$/4, $M_{H^{\pm}_{2}}$ = $M_{H^{\pm}_{1}}$ +' \
+                   + str(mass_differ)+ 'GeV' )
+    plt.legend(('Type II tan$\\beta =$ 2 2HDM', 'Type II tan$\\gamma =$ 2 3HDM ',\
+                    'Type II tan$\\gamma =$ 4 3HDM ','Type II tan$\\gamma =$ 7 3HDM ',\
+                    'Type II tan$\\gamma =$ 10 3HDM ','Type II tan$\\gamma =$ 20 3HDM '),\
+               loc='upper right', shadow=True,prop={'size': 7.5} )    
 #    for n in np.arange(0,len(axs)):
         
 #        y22_axis3hdm= BR_B_Xs_gamma(mb,mw,x_axis,x_axis + 20,\
 #                        Y2(*array4()[n]),complexyfunction(*array4()[n]),\
 #                        Y3(*array4()[n]),complexyfunction3(*array4()[n])) 
 #        plt.plot(x_axis,y22_axis3hdm / (1e-4))
-        plt.axis([100.0, 1000.0, 1.0, 6.0])
+    plt.axis([100.0, 1000.0, 1.0, 6.0])
     return
 ########################################################
 Plot_3()
